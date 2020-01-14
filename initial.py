0fd0667e7a8118490730c6db3d3769fac94b2cee
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-14 21:18:52 +0600

    txt to csv done

--- initial.py
+++ initial.py
@@ -7,14 +7,36 @@ Created on Sun Jan 12 21:08:24 2020
 
 import csv
 
-with open('cleveland.data') as input_file:
-       lines = input_file.readlines()
+with open('clev.txt') as input_file:
+       lines = input_file
        newLines = []
+       newRows = []
+       Rows = []
        for line in lines:
-          newLine = line.strip().split('name')
-          newLines.append( newLine )
-          #print(newLines)
-    
+         
+          newLine = line.split()
+          for i in newLine:
+              if i=='name':
+                  newLines.append(newRows)
+                  newRows=[]
+                  
+                 
+              else:
+                  newRows.append( i )
+          
+#          print newLines
+#          print '\n'
+               
+       
+       
+              
+       
+          
+     
+
+   
 with open('output.csv', 'wb') as test_file:
        file_writer = csv.writer(test_file)
        file_writer.writerows( newLines )
+
+
