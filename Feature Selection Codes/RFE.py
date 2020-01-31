d1b0590a395f55aca21b2002a47359e6af0b38c6
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-31 23:55:19 +0600

    Edited slightly

--- Feature Selection Codes/RFE.py
+++ Feature Selection Codes/RFE.py
@@ -8,6 +8,7 @@ Created on Thu Jan 30 10:12:49 2020
 from pandas import read_csv
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 # load data
 url = 'F:/Dataset/Dataset Version 4/csv_result-cleveland.csv'
 names = ['id','age','sex','cp','trestbps','htn','chol','cigs','years','fbs','famhist','restecg','ekgmo','ekgday','ekgyr','dig','prop','nitr','pro','diuretic','thaldur','thaltime','met','thalach','thalrest','tpeakbps','tpeakbpd','trestbpd','exang','xhypo','oldpeak','slope','ca','thal','cmo','cday','cyr','num','lmt','ladprox','laddist','cxmain','om1','rcaprox','rcadist','lvx3','lvx4','lvf']
@@ -22,12 +23,12 @@ fit = rfe.fit(X, Y)
 print("Num Features: %d" % fit.n_features_)
 print("Selected Features: %s" % fit.support_)
 print("Feature Ranking: %s" % fit.ranking_)
-l=[]
-l=fit.ranking_
-print (l)
-
-for i in l:
-    #print i
-    if i==1:
-        #print 'ok'
-        print 
+#l=[]
+#l=fit.ranking_
+#print (l)
+#
+#for i in l:
+#    #print i
+#    if i==1:
+#        #print 'ok'
+#        print 
