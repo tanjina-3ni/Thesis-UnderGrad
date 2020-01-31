e721bdcd6ad90e05c5b36fca0a4942c98c9fb29c
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-31 23:54:05 +0600

    plotted

--- Feature Selection Codes/univariate_f_classif.py
+++ Feature Selection Codes/univariate_f_classif.py
@@ -8,6 +8,8 @@ Created on Thu Jan 30 16:29:47 2020
 import pandas as pd
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif
+import matplotlib.pyplot as plt
+
 data = pd.read_csv('F:/Dataset/Dataset Version 4/csv_result-cleveland (2).csv')
 X = data.iloc[:,0:47]  #independent columns
 y = data.iloc[:,-1]    #target column i.e price range
@@ -19,4 +21,13 @@ dfcolumns = pd.DataFrame(X.columns)
 #concat two dataframes for better visualization 
 featureScores = pd.concat([dfcolumns,dfscores],axis=1)
 featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-print(featureScores.nlargest(14,'Score'))  #print 10 best features
+  #print 10 best features
+
+l=[]
+l=featureScores.nlargest(14,'Score')
+print(l)
+#featureScores.nlargest(14,'Score')
+ax = plt.gca()
+l.plot(kind='bar',x='Specs',y='Score',ax=ax)
+#
+#plt.show()
