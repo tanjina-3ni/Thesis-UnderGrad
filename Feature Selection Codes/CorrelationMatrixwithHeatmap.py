9cf87c6c1474fb7e8c415110ba9806b332ff5186
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-02-24 21:19:12 +0600

    For correlation

--- Feature Selection Codes/CorrelationMatrixwithHeatmap.py
+++ Feature Selection Codes/CorrelationMatrixwithHeatmap.py
@@ -10,12 +10,12 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('F:/Dataset/Dataset Version 4/csv_result-cleveland (2).csv')
+data = pd.read_csv('F:/Dataset/Dataset Version 4/csv_result-cleveland (5).csv')
 X = data.iloc[:,0:47]  #independent columns
-y = data.iloc[:,-1]    #target column i.e price range
+y = data.iloc[:,-1]    #target column 
 #get correlations of each features in dataset
-corrmat = data.corr()
+corrmat = data.corr(method="pearson")
 top_corr_features = corrmat.index
-plt.figure(figsize=(47,47))
+plt.figure(figsize=(40,40))
 #plot heat map
 g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
