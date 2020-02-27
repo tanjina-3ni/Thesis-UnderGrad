1d496b27443e282942e2cd7dfdf7d17fe4659bdc
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-02-27 10:43:21 +0600

    Codes I have used

---
+++ CodesIhaveUsed/initialv2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 17:27:01 2020
+
+@author: Aspire
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 12 21:08:24 2020
+
+@author: Aspire
+"""
+
+import csv
+
+with open('hungarian.data') as input_file:
+       lines = input_file
+       newLines = []
+       newRows = []
+       Rows = []
+       for line in lines:
+         
+          newLine = line.split()
+          
+          for i in newLine:
+              if i=='name':
+                  newLines.append(newRows)
+                  newRows=[]
+                  
+              elif i=='-9':
+                  i='NaN'
+                  newRows.append( i )
+                  
+              else:
+                  newRows.append( i )
+          
+          print newLines
+          print '\n'
+               
+       
+       
+              
+       
+          
+     
+
+   
+with open('output.csv', 'wb') as test_file:
+       file_writer = csv.writer(test_file)
+       file_writer.writerows( newLines )
+
+
