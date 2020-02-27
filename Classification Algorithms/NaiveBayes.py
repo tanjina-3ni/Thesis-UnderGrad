7ac832ab1933163e9e158c700ada46cc79e952ed
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-02-28 00:41:23 +0600

    Naive Bayes

---
+++ Classification Algorithms/NaiveBayes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 09:10:55 2020
+
+@author: Aspire
+"""
+
+import pandas as pd
+
+data = pd.read_csv('F:/Dataset/Dataset Version 4/csv_result-cleveland (corr).csv')
+
+X =  data.drop('num', axis=1) #Drop specified labels from rows or columns. axis=1, col
+target = data['num']
+
+# splitting X and y into training and testing sets 
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.3, random_state=1) 
+  
+# training the model on training set 
+from sklearn.naive_bayes import GaussianNB 
+gnb = GaussianNB() 
+gnb.fit(X_train, y_train) 
+  
+# making predictions on the testing set 
+y_pred = gnb.predict(X_test) 
+  
+# comparing actual response values (y_test) with predicted response values (y_pred) 
+from sklearn import metrics 
+print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+
+
