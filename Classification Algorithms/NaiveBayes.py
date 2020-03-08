05e955d399a21a3e59a571a5aeb8926ad5ec981c
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-03-08 11:13:27 +0600

    confusion matrix plot

--- Classification Algorithms/NaiveBayes.py
+++ Classification Algorithms/NaiveBayes.py
@@ -10,7 +10,7 @@ import pandas as pd
 data = pd.read_csv('F:/Dataset/Dataset Version 4/csv_result-cleveland (corr).csv')
 
 X =  data.drop('num', axis=1) #Drop specified labels from rows or columns. axis=1, col
-target = data['num']
+target = data.iloc[:,-1]
 
 # splitting X and y into training and testing sets 
 from sklearn.model_selection import train_test_split 
@@ -29,8 +29,21 @@ y_pred = gnb.predict(X_test)
 from sklearn import metrics 
 print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
 
+
+
 from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sn
 
-print(confusion_matrix(y_test, y_pred))
 
-print(classification_report(y_test, y_pred))
+mat=(confusion_matrix(y_test, y_pred))
+#print mat
+df_cm = pd.DataFrame(mat, columns=np.unique(y_test), index = np.unique(y_test))
+df_cm.index.name = 'Actual'
+df_cm.columns.name = 'Predicted'
+plt.figure(figsize = (5,3))
+sn.set(font_scale=1.2)#for label size
+sn.heatmap(df_cm, cmap="Blues", annot=True,annot_kws={"size": 15})# font size
+#print(classification_report(y_test, y_pred))
+
