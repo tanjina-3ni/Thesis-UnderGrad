5ef6d9877ea244ca1e2f16059695ae3ec0ae10de
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-08-26 22:11:30 +0600

    ROC curve added

--- Classification Algorithms/LogisticRegression.py
+++ Classification Algorithms/LogisticRegression.py
@@ -51,3 +51,21 @@ print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(lo
 
 confusionmatrix(y_test, y_pred)
 
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
