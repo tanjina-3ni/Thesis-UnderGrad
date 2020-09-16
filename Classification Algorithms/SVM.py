004fef1bc7e7c80ef33a638500ea3ff701c8bae6
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-09-16 20:33:56 +0600

    ROC curve

--- Classification Algorithms/SVM.py
+++ Classification Algorithms/SVM.py
@@ -63,6 +63,26 @@ y_pred = clf.predict(X_test)
 # comparing actual response values (y_test) with predicted response values (y_pred) 
 confusionmatrix(y_test, y_pred)
 
+# %%
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+
+logit_roc_auc = roc_auc_score(y_test, clf.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, clf.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='SVM (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
 
 # %%
 from sklearn.model_selection import cross_val_score
